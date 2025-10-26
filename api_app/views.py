4ee13e455282eb52ae57096706c5a235f63f2da1
Aria-707 <arirg100@gmail.com>
2025-10-26 17:51:53 -0500

    Resolver conflictos

--- api_app/views.py
+++ api_app/views.py
@@ -1,4 +1,3 @@
-# Prueba ramaSharon :D
 # src/api_app/views.py - SIN VERIFICACIÓN DE TOKEN, CON FILTRADO POR UID
 from rest_framework.views import APIView
 from rest_framework.response import Response
